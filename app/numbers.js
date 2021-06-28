291cd44d79b16f4f3844359b8980a76a343b77c0
thedudeman69 <christopher.jp.newell@gmail.com>
2021-06-28 16:15:57 -0600

    numbers & strings complete

--- app/numbers.js
+++ app/numbers.js
@@ -1,22 +1,24 @@
 // Write a function that uses the addition operator to add two numbers together
 function add(x, y) {
-
+  return (x+y)
 }
 
 // Write a function that returns the Cubed value of x
 function cubed(x) {
-
+  return (x*x*x)
 }
 
 // Write a function that will divide two numbers, and will format the returned value
 // as a whole number. (No Decimals)
 function divideWholey(x, y) {
-
+  return Math.floor(x/y)
 }
 
 // Write a function that takes in a number and determines if it is even. 
 // It should return a boolean value - true if it is even, false if it is odd.
 function isEven(num) {
+  if (num % 2 === 0) { return true; } 
+  if (num % 1 === 0) { return false; }
 }
 
 
@@ -24,4 +26,5 @@ function isEven(num) {
 // Make it inclusive so that the starting and ending numbers are part 
 // of the range.HINT ** Utilize Javascript's Math Class (To the Google!)
 function numberGenerator(num1, num2) {
+    return Math.floor(Math.random() * (num1 - num2 + 1)) + num2;
 }
