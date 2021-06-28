40625e5273278ed2c2b8042f0ec6bee8bdc04cea
ChrisNewelldev <84216346+ChrisNewelldev@users.noreply.github.com>
2021-06-28 13:49:53 -0600

    Initial commit

---
+++ app/objects.js
@@ -0,0 +1,52 @@
+// Dog Challenges
+
+// Add the following properties to the dog object: name, legs, tail, and likes. 
+// The name property should have a value that is a string, the legs property 
+// should have a value that is a number, the tail property should have a 
+// boolean value, and the likes property should have a value that is an 
+// array.Inside the array, include at least 4 values.
+let goodDoggo = {
+  //your code in here
+}
+
+
+// write a function that returns the value of the legs property on your dog object using dot notation.
+function howManyLegs() {
+  // your code inside here
+  // don't forget to use the return keyword
+}
+
+// Cat Challenges
+
+let cat = {
+  name: "Pickles",
+  legs: 4,
+  whiskers: true,
+  likes: ["napping", "playing", "running at 2:00am", "pets", "stealing food off the counter"]
+}
+
+// Write a function that uses bracket notation to change the cat's name from 
+// "Pickles" to "Kevin".Then use either dot or bracket notation to return 
+// the value of the name property.
+function renameCat() {
+  // your code inside here
+}
+
+// Create a function that uses dot or bracket notation to create a property 
+// on the cat object called "color" and assign it to any color you'd like. 
+// Then return the value of the color property.
+
+function colorTheCat() {
+  // your code inside here
+}
+
+// Delete the whiskers property off of the cat object. You may use either dot or bracket notation.
+
+
+// Write a function that uses dot AND bracket notation to access and return the 
+// value of the fourth item in the likes array("pets"). 
+// Example: objectName.property[indexNumber]
+
+function returnIndex(cat) {
+  // your code inside here
+}
---
+++ app/strings.js
@@ -0,0 +1,12 @@
+// Write a function that returns the character from a string at a given position
+function letterAtPosition(str, position) {
+
+}
+
+// Write a function that will join two strings with an optional seperating character 
+function concat(str1, str2, seperator = " ") {
+}
+
+// Write a function that will capitalize the letter at a given position and return it
+function capitalizer(str, position) {
+}
